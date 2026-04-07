6f79b1b9c9779d1fcdcd3242c15ee2ebab29f44e
Vitor Matheus de Moraes <vitor.moraes@dmsoft.com.br>
2026-04-07 17:28:43 -0300

    fix: update api base url to match local environment

--- src/services/api.ts
+++ src/services/api.ts
@@ -5,5 +5,5 @@ import axios from "axios";
 // no futuro de forma global, sem precisar alterar cada chamada separadamente.
 export const api = axios.create({
   // TODO: Ajustar para a porta correta que o backend está rodando localmente
-  baseURL: "https://localhost:7000/api",
+  baseURL: "http://localhost:5175/api",
 });
